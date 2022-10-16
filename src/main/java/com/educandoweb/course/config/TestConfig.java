297ab34d3a43fd31e9dbe4a4a543559039ec8feb
Igor Ferraz <igoorferraz23@gmail.com>
2022-10-16 13:11:10 -0300

    Chore: implements test to insert users manually

---
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -0,0 +1,26 @@
+package com.educandoweb.course.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Configuration
+@Profile("test") //execute only with accessed by profile defined in src/main/resources/application.properties
+public class TestConfig implements CommandLineRunner{
+	@Autowired
+	private UserRepository UserRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		
+		UserRepository.saveAll(Arrays.asList(u1, u2));
+	}
+}

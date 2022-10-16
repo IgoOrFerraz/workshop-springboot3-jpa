4121ec44bc57256565ae0d00adb4952b259e5514
Igor Ferraz <igoorferraz23@gmail.com>
2022-10-16 15:56:40 -0300

    Chore: implements service layer

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,17 +1,31 @@
 package com.educandoweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 @RestController //necessary to identify like a rest controller
 @RequestMapping(value="/users") //identified by url localhost:8080/user
 public class UserResource {
+	@Autowired
+	private UserService service;
+	
 	@GetMapping //used to response a get method
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Igor", "igor@gmail.com", "11111", "11111");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}") //used to response a get method
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
